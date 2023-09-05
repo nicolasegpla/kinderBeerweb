d5efd604baf7ed58451642e67d3fe68ee2c799fc
nicolasegpla <nicolasedixongarciapineros@gmail.com>
2023-09-04 22:29:55 -0500

    logo commit

--- src/components/NavMobile.jsx
+++ src/components/NavMobile.jsx
@@ -1,9 +1,10 @@
 import { useState } from 'react'
+import logoKinderBeer from '../assets/logo small kinder beer.png'
 function NavMobile() {
     return (
         <>
             <nav className='nav'>
-                <img className='nav__logo' src="/src/assets/logo small kinder beer.png" alt="Logo de la empresa kinder beer" />
+                <img className='nav__logo' src={logoKinderBeer} alt="Logo de la empresa kinder beer" />
                 <img className='nav__menu' src="/src/assets/menu-kinder-beer.svg" alt="menu desplegable" />
         
             </nav>
